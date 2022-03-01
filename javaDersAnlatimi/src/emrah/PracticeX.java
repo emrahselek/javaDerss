96c42e567fe97b1c0bdce16f84089509f8e68be4
emrahselek <emrahselek10@gmail.com>
2022-03-01 00:26:29 -0500

    mac

--- javaDersAnlatimi/src/emrah/PracticeX.java
+++ javaDersAnlatimi/src/emrah/PracticeX.java
@@ -1,4 +1,45 @@
 package emrah;
 
+import java.util.Scanner;
+
 public class PracticeX {
+
+    //prime number
+
+    public static void main(String[] args){
+
+        int emrah=scano();
+        boolean opopo=primeNumberMn(emrah);
+
+        System.out.println(opopo);
+
+    }
+
+    public static int scano(){
+        Scanner scannn=new Scanner(System.in);
+        System.out.println("gir loo");
+        int user = scannn.nextInt();
+        return user;
+    }
+
+    public static boolean primeNumberMn(int number){
+        int count=0;
+        boolean sonuc=true;
+        for(int i=2; i<number; i++){
+            if(number % i ==0){
+                count++;
+            }
+        }
+        if(number<=0){
+            sonuc=false;
+        }else if(number==1 || count==0){
+            sonuc=true;
+        }else{
+            sonuc=false;
+        }
+
+        return sonuc;
+
+    }
+
 }
