15b3dd42d21d530e6d927717e5ed2f10a88647aa
emrahselek <emrahselek10@gmail.com>
2022-02-28 22:13:52 -0500

    mac

--- javaDersAnlatimi/src/emrah/Day02_PrimeNumber.java
+++ javaDersAnlatimi/src/emrah/Day02_PrimeNumber.java
@@ -1,10 +1,34 @@
 package emrah;
 
+import java.util.Scanner;
+
 public class Day02_PrimeNumber {
 
         public static void main(String[] args) {
             int number = 10;
             primeNumber(number);
+
+            //prime Number
+            Scanner scan = new Scanner(System.in);
+            System.out.println("enter a positive number");
+            int mynumber= scan.nextInt();
+            int mycount=0;
+            for(int i=2; i<mynumber; i++){
+                if(mynumber % i == 0){
+                    mycount++;
+                }
+            }
+
+            if(mynumber<=0){
+                System.out.println("opsss Sorry Man. Try again later:D");
+            }else if(mynumber==1){
+                System.out.println("prime number");
+            }else if(mycount==0){
+                System.out.println("prime number");
+            }else{
+                System.out.println("NOT Prime number");
+            }
+
 
         }
         public static void primeNumber (int number){
