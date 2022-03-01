21391d402214c6deebed369121c28a4639c4f780
emrahselek <emrahselek10@gmail.com>
2022-02-28 23:13:15 -0500

    mac

--- javaDersAnlatimi/src/emrah/Day02_PrimeNumber.java
+++ javaDersAnlatimi/src/emrah/Day02_PrimeNumber.java
@@ -8,9 +8,14 @@ public class Day02_PrimeNumber {
             int number = 10;
             primeNumber(number);
 
-            //prime Number
+            //prime number conditions:
+            // must be bigger than 0,
+            // not zero,
+            // not negative number,
+            // 0 is PN,
+            // just divide by 1 and itself
             Scanner scan = new Scanner(System.in);
-            System.out.println("enter a positive number");
+            System.out.println("come on, give me your best number");
             int mynumber= scan.nextInt();
             int mycount=0;
             for(int i=2; i<mynumber; i++){
@@ -21,14 +26,11 @@ public class Day02_PrimeNumber {
 
             if(mynumber<=0){
                 System.out.println("opsss Sorry Man. Try again later:D");
-            }else if(mynumber==1){
-                System.out.println("prime number");
-            }else if(mycount==0){
+            }else if(mynumber==1 || mycount==0){
                 System.out.println("prime number");
             }else{
                 System.out.println("NOT Prime number");
             }
-
 
         }
         public static void primeNumber (int number){
