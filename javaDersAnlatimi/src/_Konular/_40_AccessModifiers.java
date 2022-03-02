dcafdb675601bde7d814205d69a822a7d648e2c7
emrahselek <emrahselek10@gmail.com>
2022-03-02 00:36:03 -0500

    mac

---
+++ javaDersAnlatimi/src/_Konular/_40_AccessModifiers.java
@@ -0,0 +1,32 @@
+package _Konular;
+
+public class _40_AccessModifiers {
+
+    /*
+    -private
+    Sadece ayni classta kullanilabilir. Baska bir classtan erisim izni vermez.
+    -default
+
+    -protected
+    -public
+
+    Degisken yada methodlara nerden erisilebilir nereden erisilemez bunu belirten belirteclerdir
+    Degislem yada methodlara nereden erisilebilir nereden erisilemez bunu belirten belirteclerdir.
+
+
+     */
+    public static void main(String[] args) {
+
+        Araba14 nesne = new Araba14();
+       // System.out.println(nesne.hiz);
+        
+
+
+    }
+
+}
+
+class Araba14{
+
+    private int hiz = 200;
+}
